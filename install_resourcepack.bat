edf0c8119f9eae26b6d71d8f3251673a49f05b64
Xen <klimovjeka0987@yandex.ru>
2018-11-03 19:18:56 +0300

    update mods

--- install_resourcepack.bat
+++ install_resourcepack.bat
@@ -10,7 +10,7 @@ setlocal
 :VARIABLES
 
 rem EINSTEIN PICTURE (URL)
-SET "FILE_URL=https://github.com/XenProject/MinecraftResourcePack/raw/master/Xen_PureBDcraft_64x_MC112.zip"
+SET "FILE_URL=https://gitlab.com/xen0987/minecraftresourcepack/raw/master/Xen_PureBDcraft_64x_MC112.zip"
 
 rem SAVE FILE WHERE THE SCRIPT IS LOCATED (DEFAULT)
 SET "SAVE_TO=%~dp0"
