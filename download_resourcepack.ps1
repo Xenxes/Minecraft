5db6b2abd36818e91c193b23a84f794e799ea548
Xen <klimovjeka0987@yandex.ru>
2018-11-04 00:40:17 +0300

    fix current dir

--- download_resourcepack.ps1
+++ download_resourcepack.ps1
@@ -1,5 +1,5 @@
 $url = "https://gitlab.com/xen0987/minecraftresourcepack/raw/master/Xen_PureBDcraft_64x_MC112.zip"
-$output = "$PSScriptRoot\resourcepacks\Xen_PureBDcraft_64x_MC112.zip"
+$output = "$pwd\resourcepacks\Xen_PureBDcraft_64x_MC112.zip"
 $start_time = Get-Date
 
 Import-Module BitsTransfer
